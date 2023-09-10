abd947cfda8bfb891621cc70cb21462d39b9e220
AnPhungVuong <122555894+AnPhungVuong@users.noreply.github.com>
2023-09-10 17:14:42 -0400

    add sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import {}
+
 const App = () => (
-  <main>
-    
+  <main className="relative">
+    nav
+    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+    <section className="padding">popular product</section>
+    <section className="padding">super quality </section>
+    <section className="padding" py-10>service </section>
+    <section className="padding">special offer </section>
+    <section className="padding bg-pale-blue">customer review </section>
+    <section className="padding-x sm:py-32 py-16 w-full">subscribe </section>
+    <section className="bg-black padding-x padding -t pb-8">footer </section>
+
   </main>
 );
 
