d700c3e92af9f99cd09cb09f29d40b8ccaadc2a1
AnPhungVuong <122555894+AnPhungVuong@users.noreply.github.com>
2023-09-10 17:24:28 -0400

    structure finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import {}
+import { CustomerReviews, Hero ,SuperQuality, Services, SpecialOffer,Subscribe, Footer,PopularProducts } from "./sections";
 
 const App = () => (
-  <main className="relative">
-    nav
-    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
-    <section className="padding">popular product</section>
-    <section className="padding">super quality </section>
-    <section className="padding" py-10>service </section>
-    <section className="padding">special offer </section>
-    <section className="padding bg-pale-blue">customer review </section>
-    <section className="padding-x sm:py-32 py-16 w-full">subscribe </section>
-    <section className="bg-black padding-x padding -t pb-8">footer </section>
+  <main className="relative<">
+    <Nav/>
+    <section className="xl:padding-1 wide:padding-r padding-b"><Hero/></section>
+    <section className="padding"><PopularProducts/></section>
+    <section className="padding"><SuperQuality/> </section>
+    <section className="padding" py-10><Service/> </section>
+    <section className="padding"><SpecialOffer/> </section>
+    <section className="padding bg-pale-blue"><CustomerReviews/> </section>
+    <section className="padding-x sm:py-32 py-16 w-full"><Subscribe/> </section>
+    <section className="bg-black padding-x padding -t pb-8"><Footer/> </section>
 
   </main>
 );
